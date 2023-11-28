d31202dcf2653c6a70ed69e786f9f98de7a48c71
leebernard <ldbernard@asu.edu>
2023-11-28 16:52:43 -0700

    Working on extending the script for all of Sept.

--- generate_exoplanet_list.py
+++ generate_exoplanet_list.py
@@ -6,11 +6,13 @@ from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
 from astropy.coordinates import AltAz, EarthLocation, SkyCoord, get_sun
 from astropy.time import Time
 
+verbose = True
+
 cols_needed = ('pl_name, hostname, hip_name, sy_vmag, sy_zmag, pl_bmassj, pl_bmasse, ra, dec, '
                'pl_orbper, pl_orbpererr1, pl_orbpererr2, pl_orbperstr,'
                'pl_trandep, pl_trandeperr1, pl_trandeperr2, pl_trandeplim')
 
-criteria = 'pl_trandep > 0.0 and sy_vmag < 11 and pl_orbper < 7'
+criteria = 'pl_trandep > 0.0 and sy_vmag < 11 and pl_orbper < 7'  # need k-band magnitude
 exolist = NasaExoplanetArchive.query_criteria(table='pscomppars', select=cols_needed, where=criteria)
 
 test_loc = SkyCoord(exolist['ra'][0], exolist['dec'][0], frame='icrs')
@@ -29,9 +31,10 @@ sun_loc = get_sun(time)
 print('sun location', sun_loc)
 
 # seperation should be +- 50 degrees from anti-sun azimuth
-sun_altaz = sun_loc.transform_to(AltAz(obstime=time,location=fort_sumner))
-print(f"sun's Altitude = {sun_altaz.alt:.5}")
-print(f"sun's Azimuth = {sun_altaz.az:.5}")
+# sun_altaz = sun_loc.transform_to(AltAz(obstime=time, location=fort_sumner))
+sun_altaz = sun_loc.transform_to(AltAz(obstime=sept_hours, location=fort_sumner))
+print(f"sun's Altitude = {sun_altaz.alt}")
+print(f"sun's Azimuth = {sun_altaz.az}")
 
 anti_sun = sun_altaz.az - 180*u.deg
 range = 50*u.deg
@@ -39,13 +42,20 @@ range = 50*u.deg
 min_az = anti_sun - range
 max_az = anti_sun + range
 
-exolist_azalt = [SkyCoord(target['ra'], target['dec'], frame='icrs').transform_to(AltAz(obstime=time,location=fort_sumner)) for target in exolist]
+# astronomical twilight is defined as the Sun being 12-18° below the horizon. <-18° is complete darkness
+# 12° below the horizon (-12°) is the start of observable conditions.
+# if alt < -12°, sun position does not matter
+
+exolist_azalt = [SkyCoord(target['ra'], target['dec'], frame='icrs').transform_to(AltAz(obstime=time, location=fort_sumner)) for target in exolist]
+sun_altaz.alt > -12
 is_anti_sun = np.array([max_az > target.az > min_az for target in exolist_azalt])
+is_night = np.array([])
+
 is_in_elevation = np.array([alt_upper_lim > target.alt > alt_lower_lim for target in exolist_azalt])
 
 # valid_exolist = [exoplanet for (exoplanet, valid) in zip(exolist, is_anti_sun*is_in_elevation) if valid]
 # the above is cute, but wrong. the correct way is below
-valid_exolist = exolist[is_in_elevation * is_anti_sun]
+valid_exolist = exolist[is_in_elevation and is_anti_sun]
 
 # min_sep = (180 - 50)*u.deg  # minimum target separation from the sun in degrees
 # has_min_seperation = np.array([sun_loc.separation(SkyCoord(target['ra'], target['dec'], frame='icrs')) > min_sep for target in exolist])
