10f0ebf38e350e7429e6614b4f6fe8ab9851c03d
leebernard <ldbernard@asu.edu>
2023-12-07 13:50:50 -0700

    changed the way time samples are generated to use a start and end time. Changed anti_sun variable to be unitless. Changed exotable to a query_criteria_async() call from a query_criteria() call to avoid a bug in query_criteria. Changed the validation routines ot use numpy boolean arrays. Added a validation check for the length of time the planet is observable. Added some debugging statements under a 'verbose' flag.

--- generate_exoplanet_list.py
+++ generate_exoplanet_list.py
@@ -3,41 +3,49 @@ import astropy.units as u
 import numpy as np
 
 from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
-from astropy.coordinates import AltAz, EarthLocation, SkyCoord, get_sun
+from astropy.coordinates import AltAz, EarthLocation, SkyCoord, get_sun, Longitude, Latitude
 from astropy.time import Time
 
-verbose = True
+verbose = False
 
-cols_needed = ('pl_name, hostname, hip_name, sy_vmag, sy_zmag, pl_bmassj, pl_bmasse, ra, dec, '
+cols_needed = ('pl_name, hostname, hip_name, sy_vmag, sy_kmag, pl_bmassj, pl_bmasse, ra, dec, '
                'pl_orbper, pl_orbpererr1, pl_orbpererr2, pl_orbperstr,'
                'pl_trandep, pl_trandeperr1, pl_trandeperr2, pl_trandeplim')
 
-criteria = 'pl_trandep > 0.0 and sy_vmag < 11 and pl_orbper < 7'  # need k-band magnitude
-exolist = NasaExoplanetArchive.query_criteria(table='pscomppars', select=cols_needed, where=criteria)
+criteria = 'pl_trandep > 0.0 and sy_vmag < 12 and sy_kmag < 11 and pl_orbper < 7'
+exotable = NasaExoplanetArchive.query_criteria_async(table='pscomppars', select=cols_needed, where=criteria)
 
-test_loc = SkyCoord(exolist['ra'][0], exolist['dec'][0], frame='icrs')
+test_loc = SkyCoord(exotable['ra'][0]*u.deg, exotable['dec'][0]*u.deg, frame='icrs')
 
 # location geo:34.401944,-104.194722, 4032 ft (1220 m)
 fort_sumner = EarthLocation(lat=34.401944*u.deg, lon=-104.194722*u.deg, height=1220*u.m)
-alt_upper_lim = 57*u.deg
-alt_lower_lim = 22*u.deg
+alt_upper_lim = 57  # *u.deg
+alt_lower_lim = 22  # *u.deg
+
 
 utc_offset = -6*u.hour  # Eastern Daylight Time
-time = Time('2024-9-1 09:00:00') - utc_offset
+start_time = Time('2024-9-6 14:00:00') - utc_offset
+end_time = Time('2024-9-7 08:00:00') - utc_offset
+flight_time = end_time - start_time  # this gives flight time in days
+flight_time = flight_time.to_value(u.hour)  # convert flight time to hours
+samples_per_hour = 1
+min_time_obsv = 4  # in hours
 
-sept_hours = time + np.arange(30*24)*u.hour
+# generate time samples on a per-hour basis, with the frequency determined by samples_per_hour
+sept_hours = np.linspace(start_time, end_time, num=round(flight_time * samples_per_hour))
 
-sun_loc = get_sun(time)
-print('sun location', sun_loc)
+sun_loc = get_sun(sept_hours)
 
 # seperation should be +- 50 degrees from anti-sun azimuth
 # sun_altaz = sun_loc.transform_to(AltAz(obstime=time, location=fort_sumner))
 sun_altaz = sun_loc.transform_to(AltAz(obstime=sept_hours, location=fort_sumner))
-print(f"sun's Altitude = {sun_altaz.alt}")
-print(f"sun's Azimuth = {sun_altaz.az}")
+if verbose:
+    print('sun location', sun_loc)
+    print(f"sun's Altitude = {sun_altaz.alt}")
+    print(f"sun's Azimuth = {sun_altaz.az}")
 
-anti_sun = sun_altaz.az - 180*u.deg
-range = 50*u.deg
+anti_sun = sun_altaz.az/u.deg - 180
+range = 50
 
 min_az = anti_sun - range
 max_az = anti_sun + range
@@ -46,28 +54,50 @@ max_az = anti_sun + range
 # 12° below the horizon (-12°) is the start of observable conditions.
 # if alt < -12°, sun position does not matter
 
-exolist_azalt = [SkyCoord(target['ra'], target['dec'], frame='icrs').transform_to(AltAz(obstime=time, location=fort_sumner)) for target in exolist]
-sun_altaz.alt > -12
-is_anti_sun = np.array([max_az > target.az > min_az for target in exolist_azalt])
-is_night = np.array([])
+exo_skycoord = np.array([SkyCoord(target['ra']*u.deg, target['dec']*u.deg, frame='icrs').
+                         transform_to(AltAz(obstime=sept_hours, location=fort_sumner)) for target in exotable])
+# transpose hours and targets
+exo_skycoord = exo_skycoord.transpose()
 
-is_in_elevation = np.array([alt_upper_lim > target.alt > alt_lower_lim for target in exolist_azalt])
+# sun_altaz.alt > -12
+# get_az = np.vectorize(lambda x: x.az.value)
+# exo_az = get_az(exo_skycoord)
+exo_altaz_cube = np.array([[[altaz.alt/u.deg, altaz.az/u.deg] for altaz in altaz_list] for altaz_list in exo_skycoord])
 
-# valid_exolist = [exoplanet for (exoplanet, valid) in zip(exolist, is_anti_sun*is_in_elevation) if valid]
+alt = exo_altaz_cube[:,:, 0]
+az = exo_altaz_cube[:,:,1]
+is_anti_sun = (az < max_az[0]) & (az > min_az[0])
+
+is_night = ~ (sun_altaz.alt > -12*u.deg)
+
+is_in_elevation = (alt_upper_lim > alt) & (alt > alt_lower_lim)
+
+
+# valid_exotable = [exoplanet for (exoplanet, valid) in zip(exotable, is_anti_sun*is_in_elevation) if valid]
 # the above is cute, but wrong. the correct way is below
-valid_exolist = exolist[is_in_elevation and is_anti_sun]
+# combine all the validation checks
+valid_exo_cube = is_in_elevation * (is_anti_sun + is_night[:, None])
+# find out which exoplanet candidates survived
+is_valid = valid_exo_cube.sum(axis=0) > min_time_obsv/samples_per_hour
+
+'''I need a way of calculating how long each valid target is observable'''
 
 # min_sep = (180 - 50)*u.deg  # minimum target separation from the sun in degrees
-# has_min_seperation = np.array([sun_loc.separation(SkyCoord(target['ra'], target['dec'], frame='icrs')) > min_sep for target in exolist])
+# has_min_seperation = np.array([sun_loc.separation(SkyCoord(target['ra'], target['dec'], frame='icrs')) > min_sep for target in exotable])
 
-print("Number of planets in database:", len(exolist))
-print('Number of planets observable at', time, ':', len(valid_exolist))
+valid_exo_table = exotable[is_valid]
 
-print(valid_exolist['pl_trandep'])
+verbose = True
+if verbose:
+    print("Number of planets in database:", len(exotable))
+    print('Number of planets observable between', start_time, 'and', end_time, ':', is_valid.sum())
+
+if verbose:
+    print(valid_exotable['pl_trandep'])
 
 fig, ax = plt.subplots(tight_layout=True)
 
-ax.hist(exolist['pl_trandep'].data, bins=100)
+ax.hist(exotable['pl_trandep'].data, bins=100)
 
 
 
