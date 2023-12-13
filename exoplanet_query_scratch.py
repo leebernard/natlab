45a04a3644af7446e644951724226b1d1933ab90
leebernard <ldbernard@asu.edu>
2023-12-13 14:23:24 -0700

    misc changed in the scratch files

--- exoplanet_query_scratch.py
+++ exoplanet_query_scratch.py
@@ -18,11 +18,11 @@ alt_lims = (57*u.deg, 22*u.deg)
 alt_lowerlim = alt_lims[-1]
 
 # lower limit due south at midnight
-sky_point = SkyCoord(alt=alt_lowerlim, az=180*u.deg, obstime=time, frame = 'altaz', location=fort_sumner)
+sky_point = SkyCoord(alt=alt_lowerlim, az=180*u.deg, obstime=time, frame='altaz', location=fort_sumner)
 print(sky_point.transform_to('icrs'))
 
 # upper limit due north at midnight
-sky_point = SkyCoord(alt=alt_lowerlim, az=0.0*u.deg, obstime=time, frame = 'altaz', location=fort_sumner)
+sky_point = SkyCoord(alt=alt_lowerlim, az=0.0*u.deg, obstime=time, frame='altaz', location=fort_sumner)
 print(sky_point.transform_to('icrs'))
 
 NasaExoplanetArchive.TAP_TABLES
@@ -31,7 +31,7 @@ test = NasaExoplanetArchive.query_object("WASP-18 b", table='pscomppars', select
 
 test.colnames
 
-cols_needed = ('pl_name, hostname, hip_name, sy_vmag, sy_zmag, pl_bmassj, pl_bmasse, ra, dec, '
+cols_needed = ('pl_name, hostname, hip_name, sy_vmag, sy_kmag, pl_bmassj, pl_bmasse, ra, dec, '
                'pl_orbper, pl_orbpererr1, pl_orbpererr2,'
                'pl_trandep, pl_trandeperr1, pl_trandeperr2, pl_trandeplim,'
                'pl_orbper, pl_orbperstr')
@@ -52,5 +52,34 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots(tight_layout=True)
 
 ax.hist(test3['pl_trandep'].data)
+
+
+
+
+# test code for accessing object attributes inside numpy arrays
+import numpy as np
+from datetime import date
+
+date1 = date(2012,6,30)
+date2 = date(2011,6,30)
+date3 = date(2010,6,30)
+B = np.array([date1, date2, date3])
+
+yr = lambda x: x.year
+vyr = np.vectorize(yr)
+print(vyr(B))
+# array([2012, 2011, 2010])
+print(np.average(vyr(B)))
+# 2011.0
+
+
+test_array = np.linspace(1*u.deg, 180*u.deg, num=50)
+
+test_bool = (5*u.deg < test_array) & (test_array < 50*u.deg)
+
+
+test_array = np.linspace(1, 180, num=50)
+
+test_bool = 5 < test_array < 50
 
 
