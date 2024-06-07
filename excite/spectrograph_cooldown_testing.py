661176f5c39160ddf0552e5b383b0d3a089e01bd
leebernard <ldbernard@asu.edu>
2024-06-07 12:37:30 -0700

    Made plots of the data

--- excite/spectrograph_cooldown_testing.py
+++ excite/spectrograph_cooldown_testing.py
@@ -22,7 +22,7 @@ plate_temp = temperature_data[kst_start:kst_end]
 
 cvs_sample_start = 11
 cvs_sample_end = int(kst_minutes[-1])
-voltage_slice = voltage_data[cvs_sample_start:cvs_sample_end]
+voltage_slice = voltage_data[cvs_sample_start:cvs_sample_end]  # convert to mV
 
 voltage_time = np.arange(voltage_slice.size)
 # np.searchsorted()  # finds nearest neighbor
@@ -30,10 +30,33 @@ voltage_time = np.arange(voltage_slice.size)
 # map the plate temperature data to the voltage data, using time to interpolate
 plate_temp_interp = np.interp(voltage_time, kst_minutes, plate_temp)
 
-plt.scatter(voltage_time, plate_temp_interp)
-plt.ylabel('Kelvin')
-plt.xlabel('Seconds')
+# check if the mapping was done correctly
+# plt.scatter(voltage_time, plate_temp_interp)
+# plt.ylabel('Kelvin')
+# plt.xlabel('Seconds')
 
-# fig, ax = plt.subplots(shape=(6, 8), tight_layout=True)
+fig0, ax0 = plt.subplots(figsize=(8, 6), tight_layout=True)
+ax0.plot(plate_temp_interp, voltage_slice, label='Photodiode voltage curve')
+ax0.set_xlabel('Spectrometer plate temperature (K)')
+ax0.set_ylabel('mV')
+ax0.legend()
+
+fig1, ax1 = plt.subplots(figsize=(8, 6), tight_layout=True)
+color1 = 'tab:blue'
+ax1.plot(voltage_time, voltage_slice, label='Photodiode signal warmup curve', color=color1)
+ax1.set_xlabel('Minutes')
+ax1.set_ylabel('mV', color=color1)
+# ax1.legend()
+
+ax2 = ax1.twinx()
+color2='tab:orange'
+ax2.plot(voltage_time, plate_temp_interp,  label='Temperature warmup curve', color=color2)
+ax2.set_xlabel('Minutes')
+ax2.set_ylabel('Kelvin', color=color2)
+# ax2.legend(loc=0)
+
+fig1.legend(loc=2, bbox_to_anchor=[.15, .95])  # best doesn't work
+
+
 
 
