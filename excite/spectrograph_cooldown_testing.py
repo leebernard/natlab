5f8bc911bea2d4d1274ce53a6e0a8f24bd7041cf
leebernard <ldbernard@asu.edu>
2024-07-18 11:20:43 -0700

    Added a plot of the detector stage temperature.

--- excite/spectrograph_cooldown_testing.py
+++ excite/spectrograph_cooldown_testing.py
@@ -5,7 +5,11 @@ test_data_file = 'excite/DATA_1_warmup_20230531.csv'
 voltage_data = np.loadtxt(test_data_file)  # units are mV
 
 fname = 'excite/ccc_warmupdata_run1/ccc_ls_ktemp_d4'
-temperature_data = np.fromfile(fname, dtype='float32')
+plate_tempt_data = np.fromfile(fname, dtype='float32')
+
+# ccc_ls_ktemp_d5
+fname = 'excite/ccc_warmupdata_run1/ccc_ls_ktemp_d5'
+detector_tempt_data = np.fromfile(fname, dtype='float32')
 
 fname = 'excite/ccc_warmupdata_run1/ccc_time'
 time_data = np.fromfile(fname, dtype='float32')
@@ -18,7 +22,9 @@ time_slice = time_data[kst_start:kst_end]
 time_slice = time_slice - time_slice[0]
 kst_minutes = time_slice / 60  # assuming time data is in seconds
 
-plate_temp = temperature_data[kst_start:kst_end]
+plate_temp = plate_tempt_data[kst_start:kst_end]
+
+detector_temp = detector_tempt_data[kst_start:kst_end]
 
 cvs_sample_start = 11
 cvs_sample_end = int(kst_minutes[-1])
@@ -29,6 +35,7 @@ voltage_time = np.arange(voltage_slice.size)
 
 # map the plate temperature data to the voltage data, using time to interpolate
 plate_temp_interp = np.interp(voltage_time, kst_minutes, plate_temp)
+detector_temp_interp = np.interp(voltage_time, kst_minutes, detector_temp)
 
 # check if the mapping was done correctly
 # plt.scatter(voltage_time, plate_temp_interp)
@@ -37,7 +44,7 @@ plate_temp_interp = np.interp(voltage_time, kst_minutes, plate_temp)
 
 fig0, ax0 = plt.subplots(figsize=(8, 6), tight_layout=True)
 ax0.plot(plate_temp_interp, voltage_slice, label='Photodiode voltage curve')
-ax0.set_xlabel('Spectrometer plate temperature (K)')
+ax0.set_xlabel('Detector stage temperature (K)')
 ax0.set_ylabel('mV')
 ax0.legend()
 
@@ -56,6 +63,9 @@ ax2.set_ylabel('Kelvin', color=color2)
 # ax2.legend(loc=0)
 
 fig1.legend(loc=2, bbox_to_anchor=[.15, .95])  # best doesn't work
+
+
+
 
 
 
