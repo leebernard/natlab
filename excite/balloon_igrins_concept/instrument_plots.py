1a99e76a76c3e05d41fe8a7c0bf180f1bde6da7f
leebernard <ldbernard@asu.edu>
2024-07-26 14:32:37 -0700

    Changed the bandpass definition to have explicit variables, short_limit and long_limit

--- excite/balloon_igrins_concept/instrument_plots.py
+++ excite/balloon_igrins_concept/instrument_plots.py
@@ -21,11 +21,16 @@ mk_emi_data = np.loadtxt(gemini_em_file)
 oh_em_file = 'excite/balloon_igrins_concept/rousselot2000.dat'
 oh_em_data = np.loadtxt(oh_em_file)
 
+mcmurdo_trans_file = 'excite/balloon_igrins_concept/transmmsday_40p0km.dat'
+mcmurdo_trans_data = np.loadtxt(mcmurdo_trans_file)
 
+mcmurdo_em_file = 'excite/balloon_igrins_concept/radmmsday_40p0km.dat'
+mcmurdo_em_data = np.loadtxt(mcmurdo_em_file)
 
+short_limit = 1.5
+long_limit = 2.6
 
-
-bandpass = np.array([1.5, 2.6])
+bandpass = np.array([short_limit, long_limit])
 
 id0 = np.where(mk_trans_data[:, 0] == bandpass[0])[0][0]
 id1 = np.where(mk_trans_data[:, 0] == bandpass[1])[0][0]
@@ -34,9 +39,10 @@ wavelengths = mk_trans_data[id0:id1+1, 0]
 mk_em = mk_emi_data[id0:id1+1, 1] * 1000 # convert from 1/nm to 1/um
 mk_trans = mk_trans_data[id0:id1+1, 1]
 
-idx0 = np.absolute(oh_em_data[:, 0] - 15000).argmin()
-idx1 = np.absolute(oh_em_data[:, 0] - 26000).argmin()
+idx0 = np.absolute(oh_em_data[:, 0] - short_limit*10000).argmin()  # 1.5 um, in angstroms
+idx1 = np.absolute(oh_em_data[:, 0] - long_limit*10000).argmin()  # 2.6 um, in angstroms
 oh_em = oh_em_data[id0:id1+1]
+
 
 planet_flux = B_lambda(wavelengths*1e-6 * u.m, T=2100*u.K).to(u.J/(u.s*u.um*u.m**2)) * (1.97 * 6.95700e8 /(190 * 3.0857e16) * rp_rstar)**2 * 3 # fudge factor to make it match star signal
 star_flux = B_lambda(wavelengths*1e-6 * u.m, T=6100*u.K).to(u.J/(u.s*u.um*u.m**2)) * (1.97 * 6.95700e8 /(190 * 3.0857e16))**2
@@ -61,8 +67,8 @@ ax.set_xlabel('Wavelength (um), R~100,000')
 ax.set_yscale('log')
 ax.legend()
 
-fig, ax = plt.subplots(tight_layout=True)
-ax.plot(wavelengths, mk_trans)
+# fig, ax = plt.subplots(tight_layout=True)
+# ax.plot(wavelengths, mk_trans)
 
 fig, ax = plt.subplots(tight_layout=True)
 ax.plot(oh_em[:, 0]/10000, oh_em[:, 1]*1e2)
