c42e872970be21319eabf86ee2f3a054f7d3b4fd
leebernard <ldbernard@asu.edu>
2024-09-30 16:25:23 -0700

    Started writing the calculations for the blaze function and resolution.

--- excite/balloon_igrins_concept/spectrograph_calcs.py
+++ excite/balloon_igrins_concept/spectrograph_calcs.py
@@ -19,19 +19,26 @@ d_gem = 8.1
 d_excite = 0.5
 d_giga = 1.35
 
+wl_igrins = np.linspace(1.47, 2.5, num=40000)  # bandpass wavelengths, um
+
 phi_gem = np.radians(0.34/3600)  # arcseconds to radians. Typicak k-band Gemini seeing is 0.6-0.4 arcseconds
-F_gem = 10
+F_igrins = 10
 n_grating = 3.43  # at 130 Kelvin, longward of 1 um
-# Need:
-# grating spacing sigma
-# blaze angle
-# alternate, what modes they are using
-alpha_gem = np.radians(71.56)  # they call this R3
+alpha_igrins = np.radians(71.56)  # they call this R3
 grating_density = 36.5  # number of lines/mm
 kband_orders = np.arange(98, 122+1)  # number of orders to cover k band
 hband_orders = np.arange(72, 92+1)  # number of orders to cover h band
 
 f_camera = 126.6  # mm
 px_sampling = 3.66  # pixels per wavelength element
-d1_gem = 25  # mm
+d1_igrins = 25  # mm
+
+F_2 = f_camera/d1_igrins
+
+'''Calculate the space the spectrum takes up'''
+W_igrins = d1_igrins/np.sin(alpha_igrins)
+
+# blaze_igrins =
+
+# R_igrins = wl_igrins/d_gem *
 
