Corrected the calculation of delta.

excite/balloon_igrins_concept/spectrograph_calcs.py
@@ -24,10 +24,13 @@ wl_igrins = np.linspace(1.47, 2.5, num=40000)  # bandpass wavelengths, um
 phi_gem = np.radians(0.34/3600)  # arcseconds to radians. Typicak k-band Gemini seeing is 0.6-0.4 arcseconds
 F_igrins = 10
 n_grating = 3.43  # at 130 Kelvin, longward of 1 um
-alpha_igrins = np.radians(71.56)  # they call this R3
+delta_igrins = np.radians(71.56)  # R3 grating
+# delta_r1 = np.arctan(1)  # R1 grating example
 grating_density = 36.5  # number of lines/mm
-kband_orders = np.arange(98, 122+1)  # number of orders to cover k band
-hband_orders = np.arange(72, 92+1)  # number of orders to cover h band
+hband_orders = np.arange(98, 122+1, step=2)  # number of orders to cover k band
+kband_orders = np.arange(72, 92+1)  # number of orders to cover h band
+lam_b_hband = np.array((1823.59, 1788.03, 1753.89, 1721.08, 1689.52, 1659.15, 1629.15, 1601.71, 1574.53, 1548.30, 1522.97, 1498.50, 1474.85))  # angstroms
+lam_b_kband = np.array((2467.88, 2402.10, 2339.80, 2224.62, 2171.27, 2120.49, 2072.09, 2025.92, 1981.81, 1939.65))  # angstroms
 
 f_camera = 126.6  # mm
 px_sampling = 3.66  # pixels per wavelength element
@@ -36,9 +39,20 @@ d1_igrins = 25  # mm
 F_2 = f_camera/d1_igrins
 
 '''Calculate the space the spectrum takes up'''
-W_igrins = d1_igrins/np.sin(alpha_igrins)
+# assume theta is 1 degree
+theta = np.radians(1)
+alpha_igrins = delta_igrins + theta
+beta_blaze = delta_igrins - theta
+r = np.cos(alpha_igrins)/np.cos(beta_blaze)
+# r should be ~1
+print(f'r={r:.2f}')
 
-# blaze_igrins =
+W_igrins = d1_igrins/np.sin(alpha_igrins)
+sigma_igrins = 1/grating_density
+
+
+sin_cos = hband_orders*lam_b_hband*1e-10/(2*sigma_igrins*1e-3)
 
 # R_igrins = wl_igrins/d_gem *
 
+beta_blaze = alpha_igrins - 2*theta