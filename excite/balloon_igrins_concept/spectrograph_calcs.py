93d84b3a50349d2370ab01c39e15ce512e02b6c3
leebernard <ldbernard@asu.edu>
2024-10-07 10:05:00 -0700

    Corrected the calculations for diffraction limited operation.

--- excite/balloon_igrins_concept/spectrograph_calcs.py
+++ excite/balloon_igrins_concept/spectrograph_calcs.py
@@ -23,12 +23,13 @@ wl_igrins = np.linspace(1.47, 2.5, num=40000)  # bandpass wavelengths, um
 
 phi_gem = np.radians(0.34/3600)  # arcseconds to radians. Typicak k-band Gemini seeing is 0.6-0.4 arcseconds
 F_igrins = 8.8
+f1_igrins = 250  # mm
 n_grating = 3.435  # at 130 Kelvin, longward of 1 um
 delta_igrins = np.radians(71.56)  # R3 grating
 # delta_r1 = np.arctan(1)  # R1 grating example
 grating_density = 36.5  # number of lines/mm
-hband_orders = np.arange(98, 122+1)  #, step=2)  # number of orders to cover k band
-kband_orders = np.arange(72, 92+1)  # number of orders to cover h band
+hband_orders = np.arange(98, 122+1, step=2)  # number of orders to cover k band
+kband_orders = np.arange(72, 92+1, step=2)  # number of orders to cover h band
 lam_b_hband = np.array((1823.59, 1788.03, 1753.89, 1721.08, 1689.52, 1659.15, 1629.15, 1601.71, 1574.53, 1548.30, 1522.97, 1498.50, 1474.85))  # nm
 lam_b_kband = np.array((2467.88, 2402.10, 2339.80, 2224.62, 2171.27, 2120.49, 2072.09, 2025.92, 1981.81, 1939.65))  # nm
 
@@ -40,7 +41,7 @@ F_2 = f_camera/d1_igrins
 
 '''Calculate the space the spectrum takes up'''
 # assume theta is 1 degree
-theta = np.radians(1.4)
+theta = np.radians(1.6)
 
 alpha_igrins = delta_igrins + theta
 beta_blaze = delta_igrins - theta
@@ -53,8 +54,10 @@ print(f'f1 fold mirror clearance: {fold1_clearance:.2f} mm')
 W_igrins = d1_igrins/np.sin(alpha_igrins)
 sigma_igrins = 1/grating_density
 
-R_gem = 2*d1_igrins*1e-3 * n_grating * np.tan(delta_igrins) / (phi_gem * D_gem * r_igrins)
-print(f'Resolution R={R_gem:g}')
+lam_b_test = 2*sigma_igrins*1e6 * np.sin(delta_igrins) * np.cos(theta) * n_grating/hband_orders  # in nm
+
+R_gem = lam_b_hband*1e-9 / (phi_gem * D_gem) * hband_orders / (sigma_igrins * np.cos(alpha_igrins)) * d1_igrins
+print(f'Resolution R={R_gem}')
 
 lam_blaze_igrins = 2*sigma_igrins/hband_orders * n_grating*np.sin(delta_igrins) * 1e6  # convert from mm to nm
 # note: the above calculation is slightly off from IGRINS published numbers, with the magnitude of error depending on
@@ -77,6 +80,7 @@ sigma_hires = sigma_igrins
 alpha_hires = delta_hires + theta
 beta_b_hires = delta_hires - theta
 px_pitch = 18.  # um
+F_hires = 10  # assume F/10 telescope
 
 wl_crit = 1.5e-6  # wavelength driving slit criteria
 m_crit = np.round(n_grating * sigma_hires*1e-3 / wl_crit * (np.sin(beta_b_hires) + np.sin(alpha_hires)))
@@ -88,14 +92,14 @@ print(f'hires slit wide: {np.degrees(phi_super)*3600: .2f} arcseconds')
 d1_super = R_hires * sigma_hires/m_crit * np.cos(alpha_hires)
 print(f'Hires collimated beam diameter, half meter telescope: {d1_super:.2f} mm')
 
-f2_super = d1_super / (D_super) * 2*px_pitch*1e-6 / phi_super
+f2_super = d1_super/wl_crit * 2*px_pitch
 print(f'f camera, {D_super} m telescope: {f2_super} mm')
 
 phi_giga = wl_crit/D_giga
 d1_giga = R_hires * sigma_hires/m_crit * np.cos(alpha_hires)
 print(f'Hires collimated beam diameter, 1.35 meter telescope: {d1_giga:.2f} mm')
 
-f2_giga = d1_giga / (D_giga) * 2*px_pitch*1e-6 / phi_giga
+f2_giga = d1_giga/wl_crit * 2*px_pitch
 print(f'f camera, {D_giga} m telescope: {f2_giga} mm')
 
 
