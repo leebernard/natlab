a6b958be6333e7506f064d7b26cb06fdb20decaa
leebernard <ldbernard@asu.edu>
2026-01-13 13:08:31 -0700

    Initial commit for shearing_plate_release.py

---
+++ shearing_plate_release.py
@@ -0,0 +1,7 @@
+"""
+Module for simulating the interference pattern of a linear shearing interferometer.
+
+This version is intended for release to the public domain as open source software.
+
+
+"""
