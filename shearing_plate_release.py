53b129b49ce02616318c904e3293ac4b9a268cdf
leebernard <ldbernard@asu.edu>
2026-01-15 13:35:31 -0700

    updated shearing_plate_release.py to match Nat's update.

--- shearing_plate_release.py
+++ shearing_plate_release.py
@@ -13,15 +13,16 @@ This version is intended for release to the public domain as open source softwar
 
 """
 
-from numpy import arange, zeros, sqrt, sin, cos, round, pi, arctan2, sign, exp
+from numpy import arange, zeros, ones, sqrt, sin, cos, round, pi, arctan2, sign, exp, ceil
 
-def shear_plate(Rc, R, eps, Rs, alpha=45, plate=-1, T=18.0, wedge_ang=6.35, n_idx=1.46, lam=632.8e-6, atype='coma', wfe=0, wfe_phi=0, N=203):
+def shear_plate(Rc=1.e5, D=25.4, Dmax=36.0, eps=0, Rs=50, alpha=45, plate=-1, T=18.0, wedge_ang=6.35, n_idx=1.46, lam=632.8e-6, atype='coma', wfe=0, wfe_phi=0, acenter=[0., 0.], N=203):
     """
 
     Parameters
     ----------
     Rc: radius of curvature of the wavefront
-    R: radius of the beam (in the crossplane direction)
+    D: Diameter of the beam (in the crossplane direction)
+    Dmax: Diameter of the shearing plate. This also sets the image size
     eps: fraction of central obscuration, assuming a centered, circular obscuration.
     Rs: Distance from the shearing plate to the observation plane
     alpha: angle of incidence of the beam upon the shearing plate
@@ -41,14 +42,15 @@ def shear_plate(Rc, R, eps, Rs, alpha=45, plate=-1, T=18.0, wedge_ang=6.35, n_id
     """
 
     # default shearing plate geometry's. This will over-ride passed parameters
-    if (plate==0): wedge_ang, T = 18.,6.35
-    if (plate==1): wedge_ang, T = 40.,2.6
-    if (plate==2): wedge_ang, T = 10.,13.
-    print("beam radius:", R)
+    if (plate==0): Dmax, wedge_ang, T = 15., 10., 13.
+    if (plate==1): Dmax, wedge_ang, T = 36., 18., 6.35
+    if (plate==2): Dmax, wedge_ang, T = 70., 40., 2.6
+    print("beam radius:", D)
     print("Radius of curvature:", Rc)
 
     # calculate beam divergence at the edge of the beam
-    bdiv = 2*sign(Rc) * arctan2(R, sign(Rc)*Rc)
+    Rw = D/2
+    bdiv = 2*sign(Rc) * arctan2(Rw, sign(Rc)*Rc)
     print(f"beam divergence: {bdiv: .2e}")
 
     alpha *= pi/180
@@ -62,34 +64,55 @@ def shear_plate(Rc, R, eps, Rs, alpha=45, plate=-1, T=18.0, wedge_ang=6.35, n_id
     print (f"Angle of fringes: {phi:.2f} (degrees)")
 
     # create some starting ray positions
-    R0 = R*1.5
-    x0 = (arange(N) - (N-1)/2) * R0 * 2/(N-1)
-    x = zeros((N, N), dtype='float32') + x0
+    D0 = Dmax + int(ceil(shear))
+    x0 = (arange(N) - (N-1)/2) * D0 * 2/(N-1)
+    # create the primary beam, shifting it so the center of the shearing pattern is zero
+    x = zeros((N, N), dtype='float32') + x0 + shear/2
     y = zeros((N, N), dtype='float32')
     ty = y.T + x0
-    print(f"Image pixel scale: {R0*2/(N-1)}")
+    print (f"Image size: {D0*N/(N-1):.2f} (pixel scale: {D0/(N-1):.3f})")
 
-    # Shift x-coords of the beam, to keep zero at the center of the simulation
-    x += shear/2
     # Generate the sheared reflected beam
     xp = x - shear
     yp = y + theta*Rs
     # calculate the combined phase at the observation plane for defocus
-    phase0 = pi*(shear**2 + (theta*Rs)**2) / (lam*Rc)
-    phase = 2*pi/lam*( (theta*(1.-Rs/Rc))*y + (shear/Rc)*x ) - phase0
+    phase = 2*pi/lam*( (theta*(1.-Rs/Rc))*y + (shear/Rc)*x ) - pi*(shear**2 + (theta*Rs)**2) / (lam*Rc)
 
     # give the rays some abberations
     # if ()
 
-    # calculate the patter
-    R1, R2 = R, R
-    good = (x/R1)**2 + (y/R2)**2 <= 1
+    # calculate the pattern
+    # set the outer radius of the reflected beams
+    # R1 = Dmax/2
+    # R2 = Dmax/2*cos(alpha)
+    if (D<Dmax):
+        R1 = D/2
+    else:
+        R1 = Dmax/2
 
+    if (D<Dmax/2*cos(alpha)):
+        R2 = D/2
+    else:
+        R2 = Dmax/2*cos(alpha)
+
+
+    # mask the aperture
+    aperture_mask = (x/R1)**2 + (y/R2)**2 > 1
     # mask the central obscuration, if present
     if (eps>0):
-        good[(x/(R1*eps))**2 + (y/(R2*eps))**2 <= 1] = False
-    nx = int(round(shear / (x0[1] - x0[0])))
+        aperture_mask[(x/(R1*eps))**2 + (y/(R2*eps))**2 <= 1] = True
+    norm1 = ones(x.shape)
+    norm1[aperture_mask] = 0
 
+    # and again for the sheared beam
+    aperture_mask = ((x - shear)/R1)**2 + (y/R2)**2 > 1
+    if (eps>0):
+        aperture_mask[((x - shear)/(R1*eps))**2 + (y/(R2*eps)**2) <= 1] = True
+    norm2 = ones(x.shape)
+    norm2[aperture_mask] = 0
+
+    # convert phase to intensity
+    s = 0.25*(norm1**2 + norm2**2) - 0.5*norm1*norm2*cos(phase)
 
 
 
