8179b2c0fd1a70a6267b4da1f6b996405253fc92
leebernard <ldbernard@asu.edu>
2026-01-26 16:51:41 -0700

    Updated shearing_plate_figs.py to match the newest shearing plate simulation, by removing the transpose operation.

--- shearing_plate_figs.py
+++ shearing_plate_figs.py
@@ -9,10 +9,11 @@ matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
 # play around with oap rotation
-from shear_plate_ideal import shear_plate
+# from shear_plate_ideal import shear_plate
+from shearing_plate_release import shear_plate
 
 # cmap='gist_yarg'
-cmap='twilight'
+cmap='inferno'
 crop_idn = 25
 fig_size = (8,8)
 
@@ -28,7 +29,7 @@ rot_angle = 0.0  # in degrees
 ideal_pattern = shear_plate(Rc=1.e10, atype='oap', wfe=wf_error, wfe_phi=rot_angle)
 
 ideal_fig, ax_ideal = plt.subplots(figsize=fig_size)
-ax_ideal.imshow(ideal_pattern.T[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
+ax_ideal.imshow(ideal_pattern[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
 ax_ideal.axis('off')
 ideal_fig.tight_layout()
 
@@ -37,7 +38,7 @@ ideal_fig.tight_layout()
 wf_error = 0.25
 defocus_pattern = shear_plate(Rc=1.e10, atype='defocus', wfe=wf_error, wfe_phi=rot_angle)
 defocus_fig, ax_defocus = plt.subplots(figsize=fig_size)
-ax_defocus.imshow(defocus_pattern.T[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
+ax_defocus.imshow(defocus_pattern[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
 ax_defocus.axis('off')
 defocus_fig.tight_layout()
 
@@ -46,18 +47,18 @@ defocus_fig.tight_layout()
 oap_fig, (oap0, oap45, oap90) = plt.subplots(1, 3, figsize=(18,6))
 rot_angle = 0
 oap_pattern_0 = shear_plate(Rc=1.e10, atype='oap', wfe=wf_error, wfe_phi=rot_angle)
-oap0.imshow(oap_pattern_0.T[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
+oap0.imshow(oap_pattern_0[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
 oap0.axis('off')
 
 rot_angle = 45
 oap_pattern_45 = shear_plate(Rc=1.e10, atype='oap', wfe=wf_error, wfe_phi=rot_angle)
-oap45.imshow(oap_pattern_45.T[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
+oap45.imshow(oap_pattern_45[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
 oap45.axis('off')
 
 # oap case
 rot_angle = 90
 oap_pattern_90 = shear_plate(Rc=1.e10, atype='oap', wfe=wf_error, wfe_phi=rot_angle)
-oap90.imshow(oap_pattern_90.T[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
+oap90.imshow(oap_pattern_90[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
 oap90.axis('off')
 oap_fig.tight_layout()
 
@@ -66,7 +67,7 @@ oap_fig.tight_layout()
 rot_angle = 0.0  # in degrees
 sphere_pattern = shear_plate(Rc=1.e10, atype='sa', wfe=wf_error, wfe_phi=rot_angle)
 sphere_fig, ax_sphere = plt.subplots(figsize=fig_size)
-ax_sphere.imshow(sphere_pattern.T[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
+ax_sphere.imshow(sphere_pattern[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
 ax_sphere.axis('off')
 sphere_fig.tight_layout()
 
@@ -74,7 +75,7 @@ sphere_fig.tight_layout()
 rot_angle = 0.0  # in degrees
 coma_pattern = shear_plate(Rc=1.e10, atype='coma', wfe=wf_error, wfe_phi=rot_angle)
 coma_fig, ax_coma = plt.subplots(figsize=fig_size)
-ax_coma.imshow(coma_pattern.T[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
+ax_coma.imshow(coma_pattern[crop_idn:-crop_idn, crop_idn:-crop_idn], cmap=cmap, aspect='equal')
 ax_coma.axis('off')
 coma_fig.tight_layout()
 
