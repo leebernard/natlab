0205522c8fd5a0ab0b46928405d2dd0aee9dd21e
leebernard <ldbernard@asu.edu>
2026-01-28 17:08:21 -0700

    Added routine for opening Peter's data

--- exoclock_query.py
+++ exoclock_query.py
@@ -10,7 +10,15 @@ The top level dictionary is a list of exoplanets, with each key being the exopla
 import urllib
 import json
 
+# from astropy.io import ascii
+from astropy.table import Table
+
 exoclock_planets = json.loads(urllib.request.urlopen('https://www.exoclock.space/database/planets_json').read())
 
+file_path = '/home/lee/nat_lab/excite_mission/excite_targets/'
+
+targets_filename = 'flight_2026122118.00.00_UT-6phase2A.csv'
+
+tepcat_table = Table.read(file_path+targets_filename, format='ascii.csv', header_start=10, data_start=11)
 
 
