f0340856487c0ca055e169189cf803d2cbd8a2d7
leebernard <ldbernard@asu.edu>
2026-01-28 18:09:37 -0700

    Wrote a routine for comparing EXCITE's target list to ExoClock's. This required removing some characters from EXCITE's system names, to get the formats to match.

--- exoclock_query.py
+++ exoclock_query.py
@@ -9,6 +9,7 @@ The top level dictionary is a list of exoplanets, with each key being the exopla
 
 import urllib
 import json
+import re
 
 # from astropy.io import ascii
 from astropy.table import Table
@@ -20,5 +21,25 @@ file_path = '/home/lee/nat_lab/excite_mission/excite_targets/'
 targets_filename = 'flight_2026122118.00.00_UT-6phase2A.csv'
 
 tepcat_table = Table.read(file_path+targets_filename, format='ascii.csv', header_start=10, data_start=11)
+
+print('exoclock planet names')
+print(exoclock_planets.keys())
+
+print('EXCITE target table keys')
+print(tepcat_table.colnames)
+
+exoclock_system_names = ' '.join(exoclock_planets.keys())
+excite_target_systems = tepcat_table['System']
+
+# regular expression pattern
+is_leading_0 = re.compile(r'(?<![0-9])0')
+for target_system in excite_target_systems:
+    print(target_system)
+    # print(type(target_system))
+    # strip underscore characters
+    target_system = target_system.replace('_', '')
+    target_system = re.sub(is_leading_0, '', target_system)
+    print('After removing characters:', target_system)
+    print('Is in exoclock?', target_system in exoclock_system_names)
 
 
